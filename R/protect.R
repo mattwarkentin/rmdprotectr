767491f8dce484a7d8e034493d0d11f9ef53b621
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2021-07-06 14:41:11 -0400

    Allow the rendered files to be output to any location

--- R/protect.R
+++ R/protect.R
@@ -4,31 +4,43 @@
 #'   HTML format R Markdown reports. Simply point `protect_rmd` to the location
 #'   of your file and provide a password.
 #'
-#' @param path Path to a R Markdown file.
+#' @param input Path to the input R Markdown file to be rendered.
+#' @param output Path to where the output files should be saved.
 #' @param pw Password.
-#' @param ... Additional arguments passed to `rmarkdown::render`.
+#' @param ... Additional arguments passed to `rmarkdown::render()`.
 #'
-#' @return `path`, invisibly.
+#' @return `input`, invisibly.
 #'
 #' @export
-protect_rmd <- function(path, pw, ...){
-  rmd_dir <- fs::path_dir(fs::path_real(path))
+protect_rmd <- function(
+  input,
+  output = NULL,
+  pw,
+  ...
+){
+  input <- fs::path_real(input)
+
+  if (is.null(output)) {
+    output <- fs::path_dir(input)
+  } else {
+    output <- fs::path_real(output)
+  }
 
   hash <- openssl::sha1(pw)
 
-  hash_dir <- paste0(rmd_dir, "/", hash)
+  hash_dir <- paste0(output, "/", hash)
 
-  if(!fs::dir_exists(hash_dir)) fs::dir_create(hash_dir)
+  if (!fs::dir_exists(hash_dir)) fs::dir_create(hash_dir)
 
   rmarkdown::render(
-    path,
+    input = input,
     output_file = paste0(hash_dir, "/index.html"),
     ...
   )
 
-  write_index_html(rmd_dir)
+  write_index_html(output)
 
-  invisible(path)
+  invisible(input)
 }
 
 write_index_html <- function(path) {
