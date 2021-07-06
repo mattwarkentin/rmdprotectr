ac2f61e8104b38d262957e8ff12ec8a9b83259f2
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2021-07-06 18:11:08 -0400

    Add templated styling of landing page

--- R/protect.R
+++ R/protect.R
@@ -7,6 +7,7 @@
 #' @param input Path to the input R Markdown file to be rendered.
 #' @param output Path to where the output files should be saved.
 #' @param pw Password.
+#' @param style Object returned from `stylize()`.
 #' @param ... Additional arguments passed to `rmarkdown::render()`.
 #'
 #' @return `input`, invisibly.
@@ -16,6 +17,7 @@ protect_rmd <- function(
   input,
   output = NULL,
   pw,
+  style = stylize(),
   ...
 ){
   input <- fs::path_real(input)
@@ -38,16 +40,89 @@ protect_rmd <- function(
     ...
   )
 
-  write_index_html(output)
+  write_index_html(output, style)
 
   invisible(input)
 }
 
-write_index_html <- function(path) {
+write_index_html <- function(path, style) {
   index_html <- system.file("index.html", package = "rmdprotectr")
+
+  template <- readLines(index_html)
+
+  filled <- whisker::whisker.render(
+    template = template,
+    data = style
+  )
+
   writeLines(
-    text = readLines(index_html),
+    text = filled,
     con = paste0(path, "/index.html"),
     sep = "\n"
+  )
+}
+
+#' Stylize Landing Page
+#'
+#' @description Use this to style the appearance of the landing page
+#'   for the protected Rmd file.
+#'
+#' @param header_text Header text.
+#' @param incorrect_text Incorrect password text.
+#' @param placeholder Password input placeholder.
+#' @param button_text Button text.
+#' @param font_family Font family.
+#' @param background_color Background color.
+#' @param font_color Font color.
+#' @param box_color Box color.
+#' @param btn_font_color Button font color.
+#' @param btn_bg_color Button background color.
+#' @param btn_hover_color Button hover color.
+#' @param alert_font_color Alert font color.
+#' @param alert_bg_color Alert background color.
+#' @param alert_border_color Alert border color.
+#'
+#' @return A list.
+#'
+#' @examples
+#'
+#' stylize(font_family = "Times")
+#'
+#' @export
+
+stylize <- function(
+  header_text = "Please enter the password",
+  incorrect_text = "Incorrect password",
+  placeholder = "password",
+  button_text = "Continue",
+  font_family = "-apple-system",
+  background_color = "#f2f2f2",
+  font_color = "#2d3737",
+  box_color = "#ffffff",
+  btn_font_color = "#ffffff",
+  btn_bg_color = "#228843",
+  btn_hover_color = "#1c6d36",
+  alert_font_color = "#a94442",
+  alert_bg_color = "#f2dede",
+  alert_border_color = "#ebccd1"
+) {
+  structure(
+    list(
+      header_text = header_text,
+      incorrect_text = incorrect_text,
+      placeholder = placeholder,
+      button_text = button_text,
+      font_family = font_family,
+      background_color = background_color,
+      font_color = font_color,
+      box_color = box_color,
+      btn_font_color = btn_font_color,
+      btn_bg_color = btn_bg_color,
+      btn_hover_color = btn_hover_color,
+      alert_font_color = alert_font_color,
+      alert_bg_color = alert_bg_color,
+      alert_border_color = alert_border_color
+    ),
+    class = "rmdprotectr_styling"
   )
 }
